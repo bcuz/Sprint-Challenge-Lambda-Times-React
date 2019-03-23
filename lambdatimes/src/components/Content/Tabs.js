dd65ac5e31815016ba1730cbe8a51e24e2c0cf28
adam <bcuz@users.noreply.github.com>
2019-03-23 12:33:01 -0400

    work on tab stuff

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -16,6 +16,8 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 Tabs.propTypes = {
-  tabs: PropTypes.array
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func
 }
 export default Tabs;
